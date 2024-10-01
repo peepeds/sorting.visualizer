89b65969c3c6edfd864ba0494da3f1b01cfc1a41
peepeds <edbertmac@gmail.com>
2024-10-01 21:07:16 +0700

    fix: deployment error

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
 
 
     return (
-        <div className="text-white justify-center flex items-center h-screen">
+        <div className="text-white justify-center flex items-center ">
             <div className="text-2xl max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
                 <p className="text-blue-500 font-bold text-5xl p-2">Sorting Visualizer</p>
                 <h1 className="text-5xl font-bold md:py-6 text-white">Simulator</h1>
